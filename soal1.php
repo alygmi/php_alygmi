7d6f30077b856a399ccf3778611a65e001050231
AlYgmi <nurrusulialadiyat@gmail.com>
2022-07-28 16:57:48 +0700

    last

--- soal1.php
+++ soal1.php
@@ -1,19 +1,23 @@
 <?php
-
 $jml = $_GET['jml'];
-echo "<table border=1>\n";
-for ($a = $jml; $a > 0; $a--)
-{
-   echo "<tr><td>$a</td></tr>";
-  echo "<tr>\n";
-  for ($b = $a; $b > 0; $b--)
-  {
-    echo "<td>$b</td>";
-    $total = array($b);
 
-  }
-  echo "</tr>\n";
+for ($a = $jml; $a > 0; $a--) {
+    echo "<table border=1>\n";
+    $angka = array();
+    echo "<tr>\n";
+        for ($b = $a; $b > 0; $b--) {
+            array_push($angka, $b);
+        }
+
+    $total = array_sum($angka);
+    echo "<th>total: $total \n<br></th>";
+
+    foreach ($angka as $b) {
+        echo "<td>$b</td> ";
+
+        echo "</tr>\n";
+    }
+
+    echo "\n";
+    echo "</table>";
 }
-echo "</table>";
-
-?>
