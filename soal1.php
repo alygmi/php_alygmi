136d9a52f6b5752fcf387923366b1b8a444d9058
AlYgmi <nurrusulialadiyat@gmail.com>
2022-07-28 15:09:28 +0700

    next

--- soal1.php
+++ soal1.php
@@ -4,12 +4,13 @@ $jml = $_GET['jml'];
 echo "<table border=1>\n";
 for ($a = $jml; $a > 0; $a--)
 {
-
-    echo "<th>Total : $a";
+   echo "<tr><td>$a</td></tr>";
   echo "<tr>\n";
   for ($b = $a; $b > 0; $b--)
   {
     echo "<td>$b</td>";
+    $total = array($b);
+
   }
   echo "</tr>\n";
 }
